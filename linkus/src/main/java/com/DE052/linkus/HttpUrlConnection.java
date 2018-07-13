262d6cdadee66cf65f195bb3251452d3c69c9f9d
zakels <kj.j.cha@gmail.com>
2018-07-13 00:01:14 -0400

    HttpUrlConnection initial

---
+++ linkus/src/main/java/com/DE052/linkus/HttpUrlConnection.java
@@ -0,0 +1,186 @@
+package com.DE052.linkus;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import javax.net.ssl.HttpsURLConnection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class HttpUrlConnection {
+
+    private List<String> cookies;
+    private HttpsURLConnection conn;
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static void main(String[] args) throws Exception {
+
+        String url = "https://www.purdue.edu/apps/account/cas/login?service=https%3A%2F%2Fwl.mypurdue.purdue.edu";
+        String gmail = "https://wl.mypurdue.purdue.edu";
+
+        HttpUrlConnection http = new HttpUrlConnection();
+
+        // make sure cookies is turn on
+        CookieHandler.setDefault(new CookieManager());
+
+        // 1. Send a "GET" request, so that you can extract the form's data.
+        String page = http.GetPageContent(url);
+        //System.out.println(page);
+        String postParams = http.getFormParams(page, "kcha", "hiHelloDkssud1@");
+
+        // 2. Construct above post's content and then send a POST request for
+        // authentication
+        http.sendPost(url, postParams);
+
+        // 3. success then go to gmail.
+        String result = http.GetPageContent(gmail);
+        System.out.println(result);
+    }
+
+    private void sendPost(String url, String postParams) throws Exception {
+
+        URL obj = new URL(url);
+        conn = (HttpsURLConnection) obj.openConnection();
+
+        // Acts like a browser
+        conn.setUseCaches(false);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Host", "www.purdue.edu");
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+        System.out.println(USER_AGENT);
+        conn.setRequestProperty("Accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+//        for (String cookie : this.cookies) {
+//            System.out.println(this.cookies);
+//            //conn.addRequestProperty("Cookie", cookie.split(";", 1)[0]);
+//        }
+        conn.addRequestProperty("Cookie", "JSESSIONID=BC7DEF2AEB2AE67B3C4FE3E20885B796; __atuvc=1%7C2; __atssc=google%3B1; s_fid=0457FCCE5B097C32-147D6D2BE5819655; _ga=GA1.2.1319501092.1473218070; ajs_user_id=null; ajs_group_id=null; ajs_anonymous_id=%22482abf38-e145-4373-a2c0-79a0f32e2c83%22; mp_2e05c40c5585ba977fbcdfa555a2f51d_mixpanel=%7B%22distinct_id%22%3A%20%22160fdeb07683f5-082ce4417e2f6d-4323461-1fa400-160fdeb0769fcf%22%2C%22mp_lib%22%3A%20%22Segment%3A%20web%22%2C%22%24search_engine%22%3A%20%22google%22%2C%22%24initial_referrer%22%3A%20%22https%3A%2F%2Fwww.google.com%2F%22%2C%22%24initial_referring_domain%22%3A%20%22www.google.com%22%7D; __utma=141228339.1319501092.1473218070.1525456119.1525984416.13; __utmz=141228339.1525984416.13.6.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided); BIGipServer~WEB~pool_lppwebapa01.itap.purdue.edu_web=!Mjj/1STNwKPxfO6rRDt8gwNZROleqIXCxAwJ1WwrM7h12fUw0YL+/pZ2QeDK0QDnzh6wDSaD7A==; BIGipServer~WEB~pool_www.purdue.edu_ITSP_web=!WsVSa4Im+ecZmkWrRDt8gwNZROleqMr2Y9tM83wZly+Z5uizPExKyD51Bp6/Q3Rgm9N1dLf1LA==");
+        conn.setRequestProperty("Connection", "keep-alive");
+        conn.setRequestProperty("Referer", "https://www.purdue.edu/apps/account/cas/login?service=https%3A%2F%2Fwl.mypurdue.purdue.edu");
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length", Integer.toString(postParams.length()));
+
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
+
+        // Send post request
+        DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+        wr.writeBytes(postParams);
+        wr.flush();
+        wr.close();
+
+        int responseCode = conn.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + postParams);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in =
+                new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //System.out.println(response.toString());
+
+    }
+
+    private String GetPageContent(String url) throws Exception {
+
+        URL obj = new URL(url);
+        conn = (HttpsURLConnection) obj.openConnection();
+
+        // default is GET
+        conn.setRequestMethod("GET");
+
+        conn.setUseCaches(false);
+
+        // act like a browser
+        conn.setRequestProperty("User-Agent", USER_AGENT);
+        conn.setRequestProperty("Accept",
+                "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+        conn.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        if (cookies != null) {
+            for (String cookie : this.cookies) {
+                conn.addRequestProperty("Cookie", cookie.split(";", 1)[0]);
+            }
+        }
+        int responseCode = conn.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in =
+                new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        // Get the response cookies
+        setCookies(conn.getHeaderFields().get("Set-Cookie"));
+
+        return response.toString();
+
+    }
+
+    public String getFormParams(String html, String username, String password)
+            throws UnsupportedEncodingException {
+
+        System.out.println("Extracting form's data...");
+
+        Document doc = Jsoup.parse(html);
+        //System.out.println(doc);
+        // Google form id
+        Element loginform = doc.getElementById("fm1");
+        Elements inputElements = loginform.getElementsByTag("input");
+        List<String> paramList = new ArrayList<String>();
+        for (Element inputElement : inputElements) {
+            String key = inputElement.attr("name");
+            String value = inputElement.attr("value");
+
+            if (key.equals("Email"))
+                value = username;
+            else if (key.equals("Passwd"))
+                value = password;
+            paramList.add(key + "=" + URLEncoder.encode(value, "UTF-8"));
+        }
+
+        // build parameters list
+        StringBuilder result = new StringBuilder();
+        for (String param : paramList) {
+            if (result.length() == 0) {
+                result.append(param);
+            } else {
+                result.append("&" + param);
+            }
+        }
+        //System.out.println(result.toString());
+        return result.toString();
+    }
+
+    public List<String> getCookies() {
+        return cookies;
+    }
+
+    public void setCookies(List<String> cookies) {
+        this.cookies = cookies;
+    }
+
+}
